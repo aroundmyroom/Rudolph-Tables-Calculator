9db16ce6a536b5f0c22663721fd6c63c8eab7323
AroundMyRoom <dennis@aroundmyroom.com>
2024-10-29 19:19:05 +0100

    Update db.php
    generic info about database connection, adjust it to your own needs.

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$dbc = mysqli_connect("localhost", "root", "schachner", "rudolph");
+$dbc = mysqli_connect("localhost", "username", "password", "databasename");
 if (!$dbc) {
     die("Database connection failed: " . mysqli_error($dbc));
 }
